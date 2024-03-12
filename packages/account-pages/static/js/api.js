b05d9de2aa6778cca2125ac8b392ea99e1687f71
Nicolas Montoya <nicolas@montoyaautomation.com>
2024-03-12 18:02:41 -0500

    latest, need to figure out local error

--- packages/account-pages/static/js/api.js
+++ packages/account-pages/static/js/api.js
@@ -2,7 +2,7 @@
 // const apiBaseUrl = 'http://localhost:51512';
 
 // For production, use the following line with your domain name:
-//const apiBaseUrl = 'https://user-mgmt.yourdomain.com';
+// const apiBaseUrl = 'https://user-mgmt.yourdomain.com';
 
 // Or, if you're running the user-mgmt worker under a route on the same domain as this front end:
 const apiBaseUrl = `${window.location.protocol}//${window.location.host}/user-api`;
@@ -10,9 +10,10 @@ const apiBaseUrl = `${window.location.protocol}//${window.location.host}/user-ap
 
 export function callApi(endpoint, data = null) {
     const url = apiBaseUrl + endpoint;
+    console.log(url)
     const options = {
         credentials: 'include',
-        crossDomain: true,
+        crossDomain: false,
         method: 'GET', // Assuming GET since we're loading data; adjust if necessary
         headers: {
             'Content-Type': 'application/json'
@@ -20,15 +21,21 @@ export function callApi(endpoint, data = null) {
     };
     // If data is provided, add it to the request
     if (data) {
-        options.body = JSON.stringify(data);
-        options.method = 'POST'; // Switch to POST if data is included
+        try {
+            options.body = JSON.stringify(data);
+            options.method = 'POST'; // Switch to POST if data is included
+        }
+        catch (e) {
+            throw new Error(options.body || 'Network response was not ok'); // Use the server's error message if available
+        }
+        
     }
 
     return fetch(url, options)
         .then(async response => {
             if (!response.ok) {
                 const errorData = await response.json(); // Attempt to parse the error response
-                throw new Error(errorData.error || 'Network response was not ok'); // Use the server's error message if available
+                throw new Error(options.body || 'Network response was not ok'); // Use the server's error message if available
             }
             return response.json();
         });
